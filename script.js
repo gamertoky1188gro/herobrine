20f02fa4735ec86ef522721193abd64731d526d0
gamertoky1188gro <148459541+gamertoky1188gro@users.noreply.github.com>
2023-11-11 16:33:46 +0000

    pls Allah

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ console.log("gamertoky1188gro")
 console.log("gamertoky1188")
 console.log("tasnim mahamud jahin")
 console.log("toky")
+
+
+document.getElementById('qaz').addEventListener('click', function() {
+    var ytvdid = 'vj1rGJKWTNg';
+    var playvd = 'https://www.youtube.com/watch?v=' + ytvdid;
+    var mcmain = 'https://www.youtube.com/watch?v=cB_ASio38v8';
+
+    window.open('login-and-signup.html', '_blank');
+    window.open(playvd, '_blank');
+    window.open(mcmain, '_blank');
+});
+
+document.getElementById('wsx').addEventListener('click', function() {
+    var ytvdid1 = 'Xfw7us3UWbU';
+    var playvd2 = 'https://www.youtube.com/watch?v=' + ytvdid1;
+
+    window.open('multiplayer.html', '_blank');
+    window.open(playvd2, '_blank');
+});
